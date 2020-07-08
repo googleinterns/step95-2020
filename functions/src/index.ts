5ff3205da2df6cf9ace03a84f0401b56cb8e4afe
Evangeline Grimshaw <evangeline_grimshaw@brown.edu>
2020-07-08 16:33:52 -0400

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,14 +4,7 @@ import * as express from 'express';
 import * as bodyParser from "body-parser";
 
  const firebaseConfig = {
-    apiKey: "AIzaSyBfQKMxa1azXidOZJjT8UYDm5BnU4s2bKA",
-    authDomain: "step95-2020.firebaseapp.com",
-    databaseURL: "https://step95-2020.firebaseio.com",
-    projectId: "step95-2020",
-    storageBucket: "step95-2020.appspot.com",
-    messagingSenderId: "525367632678",
-    appId: "1:525367632678:web:476053e80e5f22c6f417e7",
-    measurementId: "G-QJE1CBXKGN"
+ 
   };
   // Initialize Firebase
 admin.initializeApp(firebaseConfig);
