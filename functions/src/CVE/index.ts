4f4fd4b661d8782ea9fe112fbf3566ce4456676c
Helena <helloprincesssg@gmail.com>
2020-07-16 21:37:40 -0400

    new bugs

--- functions/src/CVE/index.ts
+++ functions/src/CVE/index.ts
@@ -40,100 +40,36 @@ app.get('/cves', (request, response) => {
 
 });
 
-//function bulletinIDHelper(id)
-function bulletinIDHelper(id:any,res:any){
-  const db = admin.database();
-  const ref = db.ref('/CVEs');
-  ref.once('value', function(snapshot) {
-    let cves = snapshot.val();
-    cves = Enumerable.from(cves)
-    .where(function (obj) { return obj.value["BulletinVersion:1"].ASB === id })
-    .select(function (obj) { return obj.value })
-    .toArray()
-    res.send(cves);
-  }).catch(error => {console.log(error)});
-}
-
-//mock data for "cves"
-// const object = {
-//   "CVE-2015-9016": {
-//    "BulletinVersion:1": {
-//     "ASB": "2018-02",
-//     "CVE": "CVE-2015-9016",
-//     "android_id": "A-63083046",
-//     "area": "Kernel",
-//     "component": "Kernel components",
-//     "fix_details": "The fix is designed to properly track active multi-queue block IO requests and simplify blk_mq_tag_to_rq.",
-//     "patch_level": "2018-02-05",
-//     "patch_links": ["https://github.com/torvalds/linux/commit/0048b4837affd153897ed1222283492070027aa9"],
-//     "published_date": "2018-01-01",
-//     "severity": "High",
-//     "subcomponent": "Multi-queue block IO",
-//     "tech_details": "In blk_mq_tag_to_rq in blk-mq.c, there is a possible use after free due to a race condition when a request has been previously freed by blk_mq_complete_request. This could lead to local escalation of privilege.",
-//     "type": "EoP",
-//     "vendor": "Upstream Linux"
-//    }
-//   },
-//   "CVE-2016-10393": {
-//    "BulletinVersion:1": {
-//     "ASB": "2018-03",
-//     "CVE": "CVE-2016-10393",
-//     "android_id": "A-68326806",
-//     "area": "Kernel",
-//     "component": "Qualcomm closed-source components",
-//     "fix_details": "",
-//     "notes": "Qualcomm AMSS bulletin: December 2016",
-//     "patch_level": "2018-03-05",
-//     "published_date": "2018-02-01",
-//     "references": ["QC-CR#1055934"],
-//     "severity": "High",
-//     "subcomponent": "Video",
-//     "tech_details": "",
-//     "vendor": "Qualcomm"
-//    }
-//   },
-//   "CVE-2016-10394": {
-//    "BulletinVersion:1": {
-//     "ASB": "2018-03",
-//     "CVE": "CVE-2016-10394",
-//     "android_id": "A-68326803",
-//     "area": "Kernel",
-//     "component": "Qualcomm closed-source components",
-//     "fix_details": "",
-//     "notes": "Qualcomm AMSS bulletin: December 2016",
-//     "patch_level": "2018-04-05",
-//     "published_date": "2018-02-01",
-//     "references": ["QC-CR#1043068"],
-//     "severity": "Critical",
-//     "subcomponent": "Secure systems group",
-//     "tech_details": "",
-//     "vendor": "Qualcomm"
-//    }
-//   }
-// };
-
-// //select cves that have "ASB": "2018-03"
-// const idd = '2018-02';
-// //Enumerable.from(object).forEach(function(obj) { console.log(obj.value["BulletinVersion:1"].ASB) });
-// const test = Enumerable.from(object)
-// .where(function (obj) { return obj.value["BulletinVersion:1"].ASB === idd })
-// .select(function (obj) { return obj.value })
-// .toArray()
-// console.log(test);
-
 function SPLIDHelper(id:any,res:any){
   const db = admin.database();
   const ref = db.ref('/CVEs');
   ref.once('value', function(snapshot) {
     let cves = snapshot.val();
     cves = Enumerable.from(cves)
-    .where(function (obj) { return obj.value["BulletinVersion:1"].patch_level === id })
+    .where(function (obj) { return obj.value.patch_level === id })
     .select(function (obj) { return obj.value })
-    .toArray()
-    res.send(cves);
+    .toArray();
+    const result = {'CVEs': cves};
+    res.send(result);
   }).catch(error => {console.log(error)});
 }
+
+function bulletinIDHelper(id:any,res:any){
+  const db = admin.database();
+  const ref = db.ref('/CVEs');
+  ref.once('value', function(snapshot) {
+    let cves = snapshot.val();
+    cves = Enumerable.from(cves)
+    .where(function (obj) { return obj.value.ASB === id })
+    .select(function (obj) { return obj.value })
+    .toArray();
+    const result = {'CVEs': cves};
+    res.send(result);
+  }).catch(error => {console.log(error)});
+}
+
 //function SPLStartHelper(id)
+
 function CVEIDHelper(id:any,res:any){
   const db = admin.database();
   const ref = db.ref('/CVEs');
@@ -142,5 +78,6 @@ function CVEIDHelper(id:any,res:any){
     res.send(cveData[id]);
   }).catch(error => {console.log(error)});
 }
+
 //function SPL1and2Helper(id1, id2)
 
--- functions/src/SPL/index.ts
+++ functions/src/SPL/index.ts
@@ -36,21 +36,50 @@ app.get('/spls', (request, response) => {
 
 function bulletinIDHelper(id:any,res:any){
   const db = admin.database();
+  console.log("db initd!")
   const ref = db.ref('/Bulletin_SPLs');
   let splData:any;
-  ref.orderByKey().equalTo(id).once('value', function(snapshot) {
-    splData = snapshot.val();
-    let output:any;
-    for(const spl of splData[id].SPLs ){
-      db.ref('/SPL_CVE_IDs').orderByKey().equalTo(spl).once('value', function(snapshot1) {
-        output = snapshot1.val();
-        console.log("output"+output);
-      }).catch(error => {console.log(error)});
-    }
-    //res.send(splData[id].SPLs);
-    res.send(output);
-  }).catch(error => {console.log(error)});
-  //for spl in data check spl_sve tree
+  //const id = "2018-04";
+  const bulletinToSplPromise = ref.orderByKey().equalTo(id).once('value');
+  const allSplPromise = bulletinToSplPromise.then((snapshot) => {
+      splData = snapshot.val();
+      let promises:Array<any> = [];
+      for(const spl of splData[id].SPLs ){
+          console.log(spl);
+          const splPromise = db.ref('/SPL_CVE_IDs').orderByKey().equalTo(spl).once('value');
+          promises.push(splPromise);
+      }
+      return Promise.all(promises) ;
+  })
+  .catch(error => {console.log("error getting spls for bulletin id: " + error)});
+
+  allSplPromise.then((result: Array<any> | void) => {
+      // return API response to user here
+      if (result !== undefined){
+        // for (const x of result) {
+        //   console.log(x.val());
+        // }
+        res.send(result);
+      }
+  });
+  // const db = admin.database();
+  // const ref = db.ref('/Bulletin_SPLs');
+  // let splData:any;
+  // ref.orderByKey().equalTo(id).once('value', function(snapshot) {
+  //   splData = snapshot.val();
+  //   console.log("splData"+splData);
+  //   console.log("array"+splData[id].SPLs);
+  //   let output:any;
+  //   for(const spl of splData[id].SPLs ){
+  //     db.ref('/SPL_CVE_IDs').orderByKey().equalTo(spl).once('value', function(snapshot1) {
+  //       output = snapshot1.val();
+  //       console.log("output"+output);
+  //     }).catch(error => {console.log(error)});
+  //   }
+  //   //res.send(splData[id].SPLs);
+  //   res.send(output);
+  // }).catch(error => {console.log(error)});
+  // //for spl in data check spl_sve tree
 }
 //function androidVersionHelper(id)
 
