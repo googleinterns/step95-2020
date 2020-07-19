92500e25414da819f9e3fdba8e901215fd633449
Helena <helloprincesssg@gmail.com>
2020-07-19 16:12:11 -0400

    Finish u-4; Revise 1,2,7 error message

--- functions/src/SPL/index.ts
+++ functions/src/SPL/index.ts
@@ -13,74 +13,31 @@ export const getSPL = functions.https.onRequest(main);
 
 app.get('/spls', (request, response) => {
 
-    const bulletinID = String(request.query.bulletinid);
-    if (bulletinID !== null){
-      bulletinIDHelper(bulletinID,response);
-      //TODO: call helper function to query for bulletin data 
-      // const ref = db.ref('/Bulletin_SPLs');
-      // ref.orderByKey().equalTo(bulletinID).once('value', function(snapshot) {
-      //   const data = snapshot.val()
-      //   //response.send(snapshot.val());
-      //   response.send(data[bulletinID].SPLs);
-      // }).catch(error => {console.log(error)});
-    }
-
+    const bulletinID = request.query.bulletinid;
     const androidVersionID = request.query.androidVersion;
-    if (androidVersionID !== null){
-      //TODO: call helper function to query for android version data
+    if (bulletinID !== null && bulletinID !== undefined){
+      bulletinIDHelper(String(bulletinID),response);
     }
-
-    //response.send(request.query.name);
+    else if (androidVersionID !== null && androidVersionID !== undefined){
+      //androidVersionHelper(String(bulletinID),response);
+    }
 
 });
 
-function bulletinIDHelper(id:any,res:any){
-  const db = admin.database();
-  console.log("db initd!")
-  const ref = db.ref('/Bulletin_SPLs');
-  let splData:any;
-  //const id = "2018-04";
-  const bulletinToSplPromise = ref.orderByKey().equalTo(id).once('value');
-  const allSplPromise = bulletinToSplPromise.then((snapshot) => {
+function bulletinIDHelper(id:string, res:any){;
+    const db = admin.database();
+    const ref = db.ref('/Bulletin_SPL');
+    let splData:any;
+    ref.orderByKey().equalTo(id).once('value', function(snapshot) {
       splData = snapshot.val();
-      let promises:Array<any> = [];
-      for(const spl of splData[id].SPLs ){
-          console.log(spl);
-          const splPromise = db.ref('/SPL_CVE_IDs').orderByKey().equalTo(spl).once('value');
-          promises.push(splPromise);
-      }
-      return Promise.all(promises) ;
-  })
-  .catch(error => {console.log("error getting spls for bulletin id: " + error)});
-
-  allSplPromise.then((result: Array<any> | void) => {
-      // return API response to user here
-      if (result !== undefined){
-        // for (const x of result) {
-        //   console.log(x.val());
-        // }
-        res.send(result);
-      }
-  });
-  // const db = admin.database();
-  // const ref = db.ref('/Bulletin_SPLs');
-  // let splData:any;
-  // ref.orderByKey().equalTo(id).once('value', function(snapshot) {
-  //   splData = snapshot.val();
-  //   console.log("splData"+splData);
-  //   console.log("array"+splData[id].SPLs);
-  //   let output:any;
-  //   for(const spl of splData[id].SPLs ){
-  //     db.ref('/SPL_CVE_IDs').orderByKey().equalTo(spl).once('value', function(snapshot1) {
-  //       output = snapshot1.val();
-  //       console.log("output"+output);
-  //     }).catch(error => {console.log(error)});
-  //   }
-  //   //res.send(splData[id].SPLs);
-  //   res.send(output);
-  // }).catch(error => {console.log(error)});
-  // //for spl in data check spl_sve tree
+      const SPLs = {SPLs : splData[id]}
+      res.send(SPLs);
+    }).catch(error => {
+      console.log("error getting spls for bulletinID: " + error)
+    });
 }
-//function androidVersionHelper(id)
 
+// function androidVersionHelper(id){
+
+// }
 
