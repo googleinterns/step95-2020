fcb957087de9cb11e28a97e291a084b643498466
nzbutler <nzbutler@google.com>
2020-07-20 16:21:31 -0500

    adds splStartHelper

--- functions/src/CVE/index.ts
+++ functions/src/CVE/index.ts
@@ -13,7 +13,6 @@ main.use(bodyParser.json());
 export const getCVE = functions.https.onRequest(main);
 
 app.get('/cves', (request, response) => {
-  
     const bulletinID = request.query.bulletinid;
     const splID = request.query.splid;
     const splStart = request.query.splstart;
@@ -29,15 +28,17 @@ app.get('/cves', (request, response) => {
     }
     else if (splStart){
       //TODO: call helper function to query for spl start data 
+      splStartHelper(String(splStart), response)
     } 
     else if (cveID){
       getCveWithCveID(String(cveID),response);
     }
     else if (spl1 && spl2){
       //TODO: call helper function for data in between spls
+      // SPL1and2Helper(SPL1, SPL2, response);
     }
-
 });
+
 
 function getCvesWithBulletinID(id:string,res:any){
   const db = admin.database();
@@ -71,7 +72,30 @@ function getCvesWithSplID(id:string,res:any){
   });
 }
 
-//function SPLStartHelper(id)
+function splStartHelper(id : string, res : any) : void {
+  var db = admin.database();
+  var ref = db.ref('/CVEs');
+
+  ref.on("value", function(snapshot) {
+    let cves = snapshot.val();
+    let cve_array : Array<any> = [];
+    
+    const cve_jsons : any = Enumerable.from(cves)
+      .where(function(obj) {return obj.value['ASB'] < id})
+      .select(function (obj){
+        return obj.value;
+      })
+
+    for (const cve of cve_jsons){
+      cve_array.push(cve);
+    }
+
+  const result = {
+    'CVEs' : cve_array
+  }
+  res.send(result);
+  }, function(error) { console.log(error);});
+}
 
 function getCveWithCveID(id:any,res:any){
   const db = admin.database();
@@ -85,4 +109,3 @@ function getCveWithCveID(id:any,res:any){
 }
 
 //function SPL1and2Helper(id1, id2)
-
