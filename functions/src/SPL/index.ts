8b178fef1fcac77b904c5f722df36ed09cddd7d7
Helena <helloprincesssg@gmail.com>
2020-07-20 15:38:57 -0400

    Revise naming; Send error message as response; Simplify if statements

--- functions/src/SPL/index.ts
+++ functions/src/SPL/index.ts
@@ -13,28 +13,29 @@ export const getSPL = functions.https.onRequest(main);
 
 app.get('/spls', (request, response) => {
 
-    const bulletinID = request.query.bulletinid;
-    const androidVersionID = request.query.androidVersion;
-    if (bulletinID !== null && bulletinID !== undefined){
-      bulletinIDHelper(String(bulletinID),response);
-    }
-    else if (androidVersionID !== null && androidVersionID !== undefined){
-      //androidVersionHelper(String(bulletinID),response);
-    }
+  const bulletinID = request.query.bulletinid;
+  const androidVersionID = request.query.androidVersion;
+
+  if (bulletinID){
+    getSplsWithBulletinID(String(bulletinID),response);
+  }
+  else if (androidVersionID){
+    //androidVersionHelper(String(bulletinID),response);
+  }
 
 });
 
-function bulletinIDHelper(id:string, res:any){;
-    const db = admin.database();
-    const ref = db.ref('/Bulletin_SPL');
-    let splData:any;
-    ref.orderByKey().equalTo(id).once('value', function(snapshot) {
-      splData = snapshot.val();
-      const SPLs = {SPLs : splData[id]}
-      res.send(SPLs);
-    }).catch(error => {
-      console.log("error getting spls for bulletinID: " + error)
-    });
+function getSplsWithBulletinID(id:string, res:any){;
+  const db = admin.database();
+  const ref = db.ref('/Bulletin_SPL');
+  let splData:any;
+  ref.orderByKey().equalTo(id).once('value', function(snapshot) {
+    splData = snapshot.val();
+    const SPLs = {SPLs : splData[id]}
+    res.send(SPLs);
+  }).catch(error => {
+    res.send("error getting spls for bulletinID: " + error)
+  });
 }
 
 // function androidVersionHelper(id){
