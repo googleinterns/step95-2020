bc2d21100e2eded3cce526c60fb384a772da2b3c
Helena <helloprincesssg@gmail.com>
2020-07-20 12:12:35 -0400

    Finish u-10

--- functions/src/SPL/index.ts
+++ functions/src/SPL/index.ts
@@ -14,12 +14,13 @@ export const getSPL = functions.https.onRequest(main);
 app.get('/spls', (request, response) => {
 
     const bulletinID = request.query.bulletinid;
-    const androidVersionID = request.query.androidVersion;
+    const androidVersion = request.query.androidVersion;
+    
     if (bulletinID !== null && bulletinID !== undefined){
       bulletinIDHelper(String(bulletinID),response);
     }
-    else if (androidVersionID !== null && androidVersionID !== undefined){
-      //androidVersionHelper(String(bulletinID),response);
+    else if (androidVersion !== null && androidVersion !== undefined){
+      androidVersionHelper(String(androidVersion),response);
     }
 
 });
@@ -30,14 +31,41 @@ function bulletinIDHelper(id:string, res:any){;
     let splData:any;
     ref.orderByKey().equalTo(id).once('value', function(snapshot) {
       splData = snapshot.val();
-      const SPLs = {SPLs : splData[id]}
+      const SPLs = {SPLs : splData[id]};
       res.send(SPLs);
     }).catch(error => {
       console.log("error getting spls for bulletinID: " + error)
     });
 }
 
-// function androidVersionHelper(id){
-
-// }
+function androidVersionHelper(version:string, res:any){
+    const db = admin.database();
+    const ref = db.ref('/AOSP_Version_ASB_CVE_IDs');
+    let bulletinData:any;
+    const aospVerToBulletinPromise = ref.orderByKey().equalTo(version).once('value')
+    const bulletinSplPromise = aospVerToBulletinPromise.then((snapshot) => {
+    bulletinData = snapshot.val();
+    let promises = [];
+    for(const bulletinID of Object.keys(bulletinData[version])){
+      const splPromise = db.ref('/Bulletin_SPL').orderByKey().equalTo(bulletinID).once('value');
+      promises.push(splPromise);
+    }
+    return Promise.all(promises);
+    });
+    bulletinSplPromise.then((bulletinSpls) => {
+    const splArray = [];
+    const bulletinIDs = Object.keys(bulletinData[version]);
+    for (let i=0; i<bulletinSpls.length; i++){
+      const bulletinSplObject = bulletinSpls[i].val();
+      const spls = bulletinSplObject[bulletinIDs[i]];
+      for (const spl of spls){
+        splArray.push(spl);
+      }
+    }
+    const SPLs = {SPLs: splArray};
+    res.send(SPLs);
+    }).catch(error => {
+      console.log("error getting spls for AndroidVersion: " + error)
+    });
+}
 
