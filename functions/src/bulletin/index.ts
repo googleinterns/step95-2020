d5a7b7c148179379208fd0cbaaf059e9c9e69b24
Helena <helloprincesssg@gmail.com>
2020-07-21 11:28:44 -0400

    Add u-11,12

--- functions/src/bulletin/index.ts
+++ functions/src/bulletin/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as bodyParser from "body-parser";
+import * as admin from 'firebase-admin';
 
 const app = express();
 const main = express();
@@ -8,17 +9,101 @@ const main = express();
 main.use(app);
 main.use(bodyParser.json());
 
-
 export const getBulletin = functions.https.onRequest(main);
 
 app.get('/bulletins', (request, response) => {
-    const bulletinID = request.query.bulletinid;
-    if (bulletinID !== null){
-      //TODO: call helper function to query for bulletin data 
-    }
-    response.send('Testing bulletin get.');
+
+  const bulletinID = request.query.bulletinid;
+  const androidVersion = request.query.androidVersion;
+
+  if (bulletinID){
+    getSplsCvesWithBulletinID(String(bulletinID),response);
+  }
+  else if (androidVersion){
+    getSplsCvesWithAndroidVersion(String(androidVersion),response);
+  }
 
 });
 
-//function bulletinIDHelper(id)
+function getSplsCvesWithBulletinID(id:string,res:any){
+  const db = admin.database();
+  const ref = db.ref('/Bulletin_SPL');
+  let splData:any;
+  const bulletinToSplPromise = ref.orderByKey().equalTo(id).once('value');
+  const allSplPromise = bulletinToSplPromise.then((snapshot) => {
+    splData = snapshot.val();
+    const promises = [];
+    for(const spl of splData[id] ){
+        const splPromise = db.ref('/SPL_CVE_IDs').orderByKey().equalTo(spl).once('value');
+        promises.push(splPromise);
+    }
+    return Promise.all(promises);
+  })
 
+  allSplPromise.then((splCveIDs) => {
+    const splCveIDList = [];
+    for (const splCveID of splCveIDs) {
+        splCveIDList.push(splCveID.val());
+    }   
+    const output = {
+        BulletinID: id,
+        SplList: splCveIDList
+    }
+    res.send(JSON.stringify(output));
+  })
+  .catch(error => {
+    res.send("error getting details for bulletinID: " + error)
+  });
+}
+
+function getSplsCvesWithAndroidVersion(version:string,res:any){
+  const db = admin.database();
+  const ref = db.ref('/AOSP_Version_ASB_CVE_IDs');
+  let bulletinData:any;
+  const aospVerToBulletinPromise = ref.orderByKey().equalTo(version).once('value')
+  const bulletinSplPromise = aospVerToBulletinPromise.then((snapshot) => {
+    bulletinData = snapshot.val();
+    const promises = [];
+    for(const bulletinID of Object.keys(bulletinData[version])){
+        const spl = db.ref('/Bulletin_SPL').orderByKey().equalTo(bulletinID).once('value');
+        promises.push(spl);
+    }
+    return Promise.all(promises);
+  });
+
+  const splPromise = bulletinSplPromise.then((bulletinSpls) => {
+    const splArray = [];
+    const bulletinIDs = Object.keys(bulletinData[version]);
+    for (let i=0; i<bulletinSpls.length; i++){
+        const bulletinSplObject = bulletinSpls[i].val();
+        const spls = bulletinSplObject[bulletinIDs[i]];
+        for (const spl of spls){
+            splArray.push(spl);
+        }
+    }
+    return splArray;
+  });
+
+  const splCvePromise = splPromise.then((splArray) => {
+    const promises = [];
+    for (const spl of splArray) {
+    const splCve = db.ref('/SPL_CVE_IDs').orderByKey().equalTo(spl).once('value');
+    promises.push(splCve);
+    }
+    return Promise.all(promises);
+  });
+
+  splCvePromise.then((splCveArray) => {
+    const splCveIDList = [];
+    for (const splCveID of splCveArray) {
+      splCveIDList.push(splCveID.val());
+    }   
+    const output = {
+      AndroidVersion: version,
+      SplList: splCveIDList
+    }
+    res.send(JSON.stringify(output));
+  }).catch(error => {
+    res.send("error getting spls and cveIDs for AndroidVersion: " + error)
+  });
+}
