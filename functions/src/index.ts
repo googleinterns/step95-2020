cfecaf3db814fd171197e9de767b5628cc0162fa
neriyahbutler <nzbutler@google.com>
2020-07-24 14:18:28 -0500

    fixes formatting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import * as express from 'express';
-import * as bodyParser from "body-parser";
+import * as bodyParser from 'body-parser';
 import * as config from './config';
 
 admin.initializeApp(config.firebaseConfig);
@@ -10,7 +10,7 @@ import * as CVEFunction from './CVE/index';
 import * as SPLFunction from './SPL/index';
 import * as bulletinFunction from './bulletin/index';
 import * as androidVersionFunction from './Android Version/index';
-import { error } from 'console';
+import {error} from 'console';
 
 const app = express();
 const main = express();
@@ -21,35 +21,45 @@ main.use(bodyParser.json());
 export const getCVEFunction = CVEFunction.getCVE;
 export const getSPLFunction = SPLFunction.getSPL;
 export const getBulletinFunction = bulletinFunction.getBulletin;
-export const getAndroidVersionFunction = androidVersionFunction.getAndroidVersion;
+export const getAndroidVersionFunction =
+  androidVersionFunction.getAndroidVersion;
 
-export const getData = functions.https.onRequest(main)
-let userToken: string = "";
+export const getData = functions.https.onRequest(main);
+let userToken = '';
 
 app.post('/data', (request: any, response: any) => {
-  if (request.body['email']){
-    const email : string = request.body['email'];
-    setAdminPriveleges(email).then(() => {
-      response.send('admin_done');
-    }).catch(error);
-  } else if(request.body['userToken']){
+  if (request.body['email']) {
+    const email: string = request.body['email'];
+    setAdminPriveleges(email)
+      .then(() => {
+        response.send('admin_done');
+      })
+      .catch(error);
+  } else if (request.body['userToken']) {
     userToken = request.body['userToken'];
-    admin.auth().verifyIdToken(userToken).then((claims) => {
-      if (claims.isAdmin === true) {console.log('user is admin');}
-      else { console.log('not admin');}
-    }).catch(error);
+    admin
+      .auth()
+      .verifyIdToken(userToken)
+      .then(claims => {
+        if (claims.isAdmin === true) {
+          console.log('user is admin');
+        } else {
+          console.log('not admin');
+        }
+      })
+      .catch(error);
   }
 });
 
-async function setAdminPriveleges(userEmail: string) : Promise<void> {
+async function setAdminPriveleges(userEmail: string): Promise<void> {
   const user = await admin.auth().getUserByEmail(userEmail);
-  if (userEmail.split('@')[1] === 'google.com'){
+  if (userEmail.split('@')[1] === 'google.com') {
     if (user.customClaims && (user.customClaims as any).isAdmin === true) {
       return;
     }
     return admin.auth().setCustomUserClaims(user.uid, {
       isAdmin: true,
-      isPartner: false
+      isPartner: false,
     });
   }
 }
