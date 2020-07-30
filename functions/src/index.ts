9eb2ed5eddce958c426abf011a7ceec9b5757fab
Evangeline Grimshaw <evangeline_grimshaw@brown.edu>
2020-07-30 11:49:44 -0400

    Add Helelna notification file into directory.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,7 +7,7 @@ import * as CVEFunction from './CVE/index';
 import * as SPLFunction from './SPL/index';
 import * as bulletinFunction from './bulletin/index';
 import * as androidVersionFunction from './Android Version/index';
-//import * as notificationFunction from './notification/index'; Helena to upload folder
+import * as notificationFunction from './notification/index'; 
 import * as uploadFunction from './upload/index';
 
 
@@ -15,8 +15,8 @@ export const getCVEFunction = CVEFunction.getCVE;
 export const getSPLFunction = SPLFunction.getSPL;
 export const getBulletinFunction = bulletinFunction.getBulletin;
 export const getAndroidVersionFunction = androidVersionFunction.getAndroidVersion;
-//export const storeEmailFunction = notificationFunction.accountCreate;
-//export const notifyNewVersionFunction = notificationFunction.notifyNewVersion;
-//export const notifyNewReleaseFunction = notificationFunction.notifyNewRelease;
+export const storeEmailFunction = notificationFunction.accountCreate;
+export const notifyNewVersionFunction = notificationFunction.notifyNewVersion;
+export const notifyNewReleaseFunction = notificationFunction.notifyNewRelease;
 
 export const getUploadFunction = uploadFunction.getUpload;
