36bc60e1d5c4fc9dda03ab4cd4194b4a4a02cb8e
HelenaSG <65558110+HelenaSG@users.noreply.github.com>
2020-07-31 23:12:28 -0400

    Update index.ts

--- functions/src/Android Version/index.ts
+++ functions/src/Android Version/index.ts
@@ -1,43 +1,89 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import * as Enumerable from "linq";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as checks from '../errorChecks';
 
-export const getAndroidVersion = functions.https.onRequest((request, response) => {
-    getSupportedAndroidVersions(response);
-  }
-);
+export const getSPL = functions.https.onRequest((request, response) => {
 
-function getSupportedAndroidVersions(res: any) {
-  const today = new Date();
-  const dd = String(today.getDate()).padStart(2, "0");
-  const mm = String(today.getMonth() + 1).padStart(2, "0");
-  const yyyy = today.getFullYear();
-  const date = yyyy + "-" + mm + "-" + dd;
+  const bulletinID = request.query.bulletinid;
+  const androidVersion = request.query.androidVersion;
 
-  const db = admin.database();
-  const ref = db.ref("/AOSP_Version_Data");
-  let androidVerData: any;
-  const androidVerDataPromise = ref.orderByKey().once("value");
-  androidVerDataPromise.then((snapshot) => {
-    androidVerData = snapshot.val();
-    const keyList = Object.keys(androidVerData);
-    const valueList: Array<object> = Object.values(androidVerData);
-    let verEndDateList = [];
-    for (let i = 0; i < keyList.length; i++) {
-    const key = keyList[i];
-    const value: object = valueList[i];
-    const obj: any = {};
-    obj.Version = key;
-    obj.Termination_Date = Object.values(value)[1];
-    verEndDateList.push(obj);
+  if (bulletinID) {
+    if (!checks.checkBulletinIDValidity(bulletinID)) {
+      response.status(400).send('Bulletin ID is malformed.');
+    }else{
+      getSplsWithBulletinID(String(bulletinID), response);
     }
-    verEndDateList = Enumerable.from(verEndDateList)
-    .where(function (obj) {return obj.Termination_Date > date;})
-    .select(function (obj) {return obj.Version;})
-    .toArray();
-    const supportedVersion = { supportedVersion: verEndDateList };
-    res.send(supportedVersion);
-  }).catch((error) => {
-    res.status(500).send("error getting supported Android Versions: " + error);
+  }
+  else if (androidVersion) {
+    if (!checks.checkAndroidVersionValidity(androidVersion)) {
+      response.status(400).send('Android Version ID is malformed.');
+    }else{
+      getSplsWithAndroidVersion(String(androidVersion), response);
+    }
+  }
+  else{
+    response.status(400).send('No valid parameters specified. Please specify a bulletin id/android version.');
+  }
+
+});
+
+function getSplsWithBulletinID(id: string, res: any) {
+  const db = admin.database();
+  const ref = db.ref('/Bulletin_SPL');
+  let splData: any;
+  const splsPromise = ref.orderByKey().equalTo(id).once('value');
+  splsPromise.then((snapshot) => {
+    splData = snapshot.val();
+    if (splData === null || splData === undefined) {
+      throw new NotFoundError('There is no SPL data associated with this bulletin in the database.');
+    }
+    const splOutput = { Spls: splData[id] }
+    res.send(splOutput);
+  }).catch(error => {
+    if(error instanceof NotFoundError){
+      res.status(404).send(error.message);
+    }else{
+      res.status(500).send('error getting SPLs for bulletinID:' + error);
+    }
   });
 }
+
+function getSplsWithAndroidVersion(version: string, res: any) {
+  const db = admin.database();
+  const ref = db.ref('/AOSP_Version_ASB_CVE_IDs');
+  let bulletinData: any;
+  const aospVerToBulletinPromise = ref.orderByKey().equalTo(version).once('value');
+  const bulletinSplPromise = aospVerToBulletinPromise.then((snapshot) => {
+    bulletinData = snapshot.val();
+    if (bulletinData === null || bulletinData === undefined) {
+      throw new NotFoundError('There are no SPLs associated with this Android Version ID in the database.');
+    }
+    const promises = [];
+    for (const bulletinID of Object.keys(bulletinData[version])) {
+      const splPromise = db.ref('/Bulletin_SPL').orderByKey().equalTo(bulletinID).once('value');
+      promises.push(splPromise);
+    }
+    return Promise.all(promises);
+  });
+  bulletinSplPromise.then((bulletinSpls) => {
+    const splArray = [];
+    const bulletinIDs = Object.keys(bulletinData[version]);
+    for (let i = 0; i < bulletinSpls.length; i++) {
+      const bulletinSplObject = bulletinSpls[i].val();
+      const spls = bulletinSplObject[bulletinIDs[i]];
+      for (const spl of spls) {
+        splArray.push(spl);
+      }
+    }
+    const splOutput = { Spls: splArray };
+    res.send(splOutput);
+  }).catch(error => {
+    if(error instanceof NotFoundError){
+      res.status(404).send(error.message);
+    }else{
+      res.status(500).send('error getting SPLs for AndroidVersion: ' + error);
+    }
+  });
+}
+
+class NotFoundError extends Error {}
